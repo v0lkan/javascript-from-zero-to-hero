f48dd002ef97243876e59851e5e51dd870994d7b
Volkan Ozcelik <me@volkan.io>
2020-09-29 07:10:42 -0700

    s02e08

---
+++ S02/E08/index.js
@@ -0,0 +1,27 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                    “Zero to Hero”
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+{
+  class GameEntity {
+    shoot() {console.log('kaboom!');}
+  }
+
+  class Spaceship extends GameEntity {}
+  class AlienSpaceship extends Spaceship {}
+
+  const zorg = new AlienSpaceship();
+
+  console.log(zorg instanceof AlienSpaceship); // true
+  console.log(zorg instanceof Spaceship); // true
+  console.log(zorg instanceof GameEntity); // true
+  console.log(zorg instanceof Object); // true
+
+  zorg.shoot(); // kaboom!
+}
